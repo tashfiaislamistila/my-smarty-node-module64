7fa73714e9d05d2418238bc8a0e01e2ea157045b
tashfiaislamistila <tashfia.cse@gmail.com>
2022-04-20 23:44:55 -0700

    my smarty node server is ready

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello from my personal over own Smarty Smarty Pant!! with auto restart')
+});
+
+const users = [
+    { id: 1, name: 'Shabana', email: 'shabana@gmail.com', phone: '017178888888' },
+    { id: 2, name: 'Shabnoor', email: 'shabnoor@gmail.com', phone: '017178888888' },
+    { id: 3, name: 'Suchorita', email: 'shuchorita@gmail.com', phone: '017178888888' },
+    { id: 4, name: 'Shuchonda', email: 'shuchonda@gmail.com', phone: '017178888888' },
+    { id: 5, name: 'Srabonti', email: 'srabonti@gmail.com', phone: '017178888888' },
+    { id: 6, name: 'Sabila', email: 'sabila@gmail.com', phone: '017178888888' },
+    { id: 7, name: 'Shohana', email: 'shohana@gmail.com', phone: '017178888888' },
+
+]
+
+app.get('/users', (req, res) => {
+    //filter by search query parameter
+    if (req.query.name) {
+        const search = req.query.name.toLowerCase();
+        const matched = users.filter(user => user.name.toLocaleLowerCase().includes(search))
+        res.send(matched);
+    }
+    else {
+
+    }
+
+    res.send(users);
+})
+app.get('/user/:id', (req, res) => {
+    console.log(req.params);
+    const id = parseInt(req.params.id);
+    const user = users.find(u => u.id === id);
+    res.send(user);
+});
+app.post('/user', (req, res) => {
+    console.log('request', req.body);
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+    res.send(user);
+})
+app.get('/fruits', (req, res) => {
+    res.send(['mango', 'apple', 'oranges'])
+});
+app.get('/fruits/mango/fazle', (req, res) => {
+    res.send('sour fazle flavour')
+})
+app.listen(port, () => {
+    console.log('Listening to port', port)
+})
